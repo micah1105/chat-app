146914a83c28af2cb9acf208985d890936ca78a8
micah1105 <153465349+micah1105@users.noreply.github.com>
2026-04-12 21:10:01 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,48 +8,77 @@ const io = new Server(server);
 
 app.use(express.static("public"));
 
+// store users + rooms
 let users = {};
+let userRooms = {};
 
-// random avatar color
-function getColor() {
-    const colors = ["#ff6b6b", "#6c5ce7", "#00d4ff", "#00ffa3", "#ffd93d"];
-    return colors[Math.floor(Math.random() * colors.length)];
-}
+// simple message memory (for now)
+let messages = {};
 
 io.on("connection", (socket) => {
 
-    socket.on("join", (username) => {
-        users[socket.id] = {
-            name: username,
-            color: getColor()
-        };
+    // LOGIN
+    socket.on("login", (username) => {
+        users[socket.id] = username;
+
+        // default room
+        userRooms[socket.id] = "general";
+        socket.join("general");
+
+        if (!messages["general"]) messages["general"] = [];
+
+        socket.emit("logged in", username);
+        socket.emit("load messages", messages["general"]);
 
         io.emit("user list", users);
     });
 
+    // JOIN ROOM
+    socket.on("join room", (room) => {
+        socket.leave(userRooms[socket.id]);
+        socket.join(room);
+
+        userRooms[socket.id] = room;
+
+        if (!messages[room]) messages[room] = [];
+
+        socket.emit("load messages", messages[room]);
+    });
+
+    // CHAT MESSAGE
     socket.on("chat message", (data) => {
-        data.time = new Date().toLocaleTimeString([], {
-            hour: "2-digit",
-            minute: "2-digit"
-        });
 
-        data.color = users[socket.id]?.color || "#fff";
+        const room = userRooms[socket.id];
 
-        io.emit("chat message", data);
+        const msg = {
+            user: users[socket.id],
+            text: data.text,
+            time: new Date().toLocaleTimeString([], {
+                hour: "2-digit",
+                minute: "2-digit"
+            })
+        };
+
+        messages[room].push(msg);
+
+        io.to(room).emit("chat message", msg);
     });
 
-    socket.on("typing", (username) => {
-        socket.broadcast.emit("typing", username);
+    // TYPING
+    socket.on("typing", () => {
+        const room = userRooms[socket.id];
+        socket.to(room).emit("typing", users[socket.id]);
     });
 
+    // DISCONNECT
     socket.on("disconnect", () => {
         delete users[socket.id];
+        delete userRooms[socket.id];
+
         io.emit("user list", users);
     });
 });
 
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
-    console.log("Server running");
+server.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
 });
