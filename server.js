967895cbb003c6d4fb980f97ad6c0fbba81b520d
micah1105 <153465349+micah1105@users.noreply.github.com>
2026-04-12 21:03:51 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,34 +1,55 @@
-// server.js
-
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-
-const io = new Server(server, {
-    cors: {
-        origin: "*"
-    }
-});
+const io = new Server(server);
 
 app.use(express.static("public"));
 
+let users = {};
+
+// random avatar color
+function getColor() {
+    const colors = ["#ff6b6b", "#6c5ce7", "#00d4ff", "#00ffa3", "#ffd93d"];
+    return colors[Math.floor(Math.random() * colors.length)];
+}
+
 io.on("connection", (socket) => {
-    console.log("A user connected");
+
+    socket.on("join", (username) => {
+        users[socket.id] = {
+            name: username,
+            color: getColor()
+        };
+
+        io.emit("user list", users);
+    });
 
     socket.on("chat message", (data) => {
+        data.time = new Date().toLocaleTimeString([], {
+            hour: "2-digit",
+            minute: "2-digit"
+        });
+
+        data.color = users[socket.id]?.color || "#fff";
+
         io.emit("chat message", data);
     });
 
+    socket.on("typing", (username) => {
+        socket.broadcast.emit("typing", username);
+    });
+
     socket.on("disconnect", () => {
-        console.log("User disconnected");
+        delete users[socket.id];
+        io.emit("user list", users);
     });
 });
 
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-    console.log("Server running on port " + PORT);
+    console.log("Server running");
 });
